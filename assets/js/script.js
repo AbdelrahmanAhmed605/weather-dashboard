748fde8cdad4f43e3cf3377f5d86f7d8b9248fc4
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-01-28 22:50:34 -0800

    Applied feature to get a city's weather given its coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,12 +18,37 @@ function userFormSubmit(event) {
 function getCityCoordinates(cityName) {
     var geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=6cb822b337f6a4741cec5e8cacad4726`;
     fetch(geocodeUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    var lat_coord = data[0].lat;
+                    var long_coord = data[0].lon;
+                    getCityWeather(lat_coord,long_coord);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to get city geodata currently! Please try again later');
+        });
+}
+
+function getCityWeather(latitude,longitude) {
+    var weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=6cb822b337f6a4741cec5e8cacad4726`;
+    fetch(weatherUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            } else {
+            alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert("Unable to get weather data currently! Please try again later");
+        });
 }
 
 userCityForm.submit(userFormSubmit);
